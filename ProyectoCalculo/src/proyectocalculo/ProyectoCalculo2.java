3283dcc839c9932f5aee182fa6a4247d81cb505a
ricardoifc <ricardoifc@me.com>
2019-05-11 22:54:01 -0500

    java f

--- ProyectoCalculo/src/proyectocalculo/ProyectoCalculo2.java
+++ ProyectoCalculo/src/proyectocalculo/ProyectoCalculo2.java
@@ -20,8 +20,7 @@ public class ProyectoCalculo {
         int x;
         int i;
         int y;
-        int f;
-        String tabla = "";
+        int limite;
         String opcion;
         System.out.println("______________________________________\n"
                 + "Eliga una de la siguentes ecuaciones\n"
@@ -38,100 +37,100 @@ public class ProyectoCalculo {
                 + "____________________________________\n"
                 + "Escriba la opcion:");
         opcion = sc.next();
-        System.out.println("valor inicial(valor menor al valor finall)");
-        i = sc.nextInt();
-        System.out.println("valor final");
-        f = sc.nextInt();
-
+        System.out.println("Cuantos numeros en x desea");
+        limite = sc.nextInt();
         switch (opcion) {
             case "1":
-                for (i = i; i <= f; i++) {
-                    x = i;
+                for (i = 0; i <= limite; i++) {
+                    System.out.println("Ingrese el valor de X");
+                    x = sc.nextInt();
                     y = 4 * (x) + 6;
                     tabla = String.format("%s\n%d\t%d ", tabla, x, y);
                 }
                 break;
             case "2":
                 System.out.println("Ecuacion 2 (2x^2+5)");
-                for (i = i; i <= f; i++) {
-                    x = i;
+                for (i = 0; i <= limite; i++) {
+                    System.out.println("Ingrese el valor de X");
+                    x = sc.nextInt();
                     y = 2 * (x * x) + 5;
                     tabla = String.format("%s\n%d\t%d ", tabla, x, y);
-
                 }
                 break;
             case "3":
                 System.out.println("Ecuacion 3 (x^3-2x+8)");
-                for (i = i; i <= f; i++) {
-                    x = i;
+                for (i = 0; i <= limite; i++) {
+                    System.out.println("Ingrese el valor de X");
+                    x = sc.nextInt();
                     y = (x * x * x) - 2 * (x) + 8;
                     tabla = String.format("%s\n%d\t%d ", tabla, x, y);
-
                 }
                 break;
             case "4":
                 System.out.println("Ecuacion 4 (√x+6)");
-                for (i = i; i <= f; i++) {
-                    float x2 = i;
-                    float y2 = (float) Math.pow((x2 + 6), (0.5));
-                    tabla = String.format("%s\n%d\t%f ", tabla, i, y2);
-
+                for (i = 0; i <= limite; i++) {
+                    System.out.println("Ingrese el valor de X");
+                    float x1 = sc.nextInt();
+                    float y2 = (float) Math.pow((x1 + 6), (0.5));
+                    tabla = String.format("%s\n%f\t%f ", tabla, x1, y2);
                 }
                 break;
             case "5":
                 System.out.println("Ecuacion 5 Log 4x+4");
-                for (i = i; i <= f; i++) {
-                    double x2 = (double)(i);
+                for (i = 0; i <= limite; i++) {
+                    System.out.println("Ingrese el valor de X");
+                    double x2 = sc.nextInt();
                     double y2 = Math.log10(x2 * 4 + 4);
-                    tabla = String.format("%s\n%d\t%f ", tabla, i, y2);
+                    tabla = String.format("%s\n%f\t%f ", tabla, x2, y2);
                 }
                 break;
             case "6":
                 System.out.println("Ecuacion 6 (8x+6)");
-                for (i = i; i <= f; i++) {
-                    x = i;
+                for (i = 0; i <= limite; i++) {
+                    System.out.println("Ingrese el valor de X");
+                    x = sc.nextInt();
                     y = 4 * (x) + 6;
                     tabla = String.format("%s\n%d\t%d ", tabla, x, y);
-
                 }
                 break;
             case "7":
                 System.out.println("Ecuacion 7 (2x^2+5)");
-                for (i = i; i <= f; i++) {
-                    x = i;
+                for (i = 0; i <= limite; i++) {
+                    System.out.println("Ingrese el valor de X");
+                    x = sc.nextInt();
                     y = 2 * (x * x) + 5;
                     tabla = String.format("%s\n%d\t%d ", tabla, x, y);
-
                 }
                 break;
             case "8":
                 System.out.println("Ecuacion 8 (4x^3-5x+13)");
-                for (i = i; i <= f; i++) {
-                    x = i;
+                for (i = 0; i <= limite; i++) {
+                    System.out.println("Ingrese el valor de X");
+                    x = sc.nextInt();
                     y = 4 * (x * x * x) - 5 * (x) + 13;
                     tabla = String.format("%s\n%d\t%d ", tabla, x, y);
-
                 }
                 break;
             case "9":
                 System.out.println("Ecuacion 9 (√x^2+6x+2)");
-                for (i = i; i <= f; i++) {
-                    float x2 = (float)(i);
-                    float y2 = (float) Math.pow(((x2 * x2) + (6 * x2)), (0.5));
-                    tabla = String.format("%s\n%d\t%f ", tabla, i, y2);
+                for (i = 0; i <= liite; i++) {
+                    System.out.println("Ingrese el valor de X");
+                    float x1 = sc.nextInt();
+                    float y2 = (float) Math.pow(((x1 * x1) + (6 * x1)), (0.5));
+                    tabla = String.format("%s\n%f\t%f ", tabla, x1, y2);
                 }
                 break;
             case "10":
                 System.out.println("Ecuacion 10 log(√2x+6)");
-                for (i = i; i <= f; i++) {
-                    double x2 = (double)(i);
+                for (i = 0; i <= limite; i++) {
+                    System.out.println("Ingrese el valor de X");
+                    double x2 = sc.nextInt();
                     double y2 = Math.log10(Math.pow(x2 * 2 + 6, 0.5));
-                    tabla = String.format("%s\n%d\t%f ", tabla, i, y2);
+                    tabla = String.format("%s\n%f\t%f ", tabla, x2, y2);
                 }
                 break;
             default:
                 System.out.println("no se ah seleccionado una ecuacion");
         }
-        System.out.printf("Valores:\nX\tY%s\n", tabla);
     }
 }
